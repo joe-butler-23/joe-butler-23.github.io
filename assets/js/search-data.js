ad2181eb033781947a3959ab280ff82e20360466
joe-butler-23 <130206285+joe-butler-23@users.noreply.github.com>
2025-04-21 20:11:54 +0000

    Deploying to gh-pages from @ joe-butler-23/joe-butler-23.github.io@038c75d4671b74403852b651283394994286ffdf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,6 +58,11 @@ ninja.data = [{
           description: "supervised &amp; unsupervised analysis of obesity risk in Latin America",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_obesity-in-latin-america/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
